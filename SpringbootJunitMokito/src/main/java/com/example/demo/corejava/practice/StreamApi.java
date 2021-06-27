fe8b9495a4af10981194bcf53ecacc5b4ecffc96
B Syam Dinesh <syamdinesh@gmail.com>
2021-06-27 22:49:43 +0530

    Java Stream API practice class- reduce method usage.
    Signed-off-by: syamd <syamdinesh@gmail.com>

--- SpringbootJunitMokito/src/main/java/com/example/demo/corejava/practice/StreamApi.java
+++ SpringbootJunitMokito/src/main/java/com/example/demo/corejava/practice/StreamApi.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Stream;
 
 /**
  * @author syam dinesh
@@ -15,7 +16,15 @@ public class StreamApi {
     public static void main(String[] args) {
         Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
         try{
-            Files.lines(Paths.get("C:\\Users\\syamd\\OneDrive\\Desktop\\edo.txt")).map(s->s.subSequence(4,s.length())).forEach(System.out::println);
+//            Files.lines(Paths.get("C:\\Users\\syamd\\OneDrive\\Desktop\\edo.txt")).map(s->s.subSequence(4,s.length())).forEach(System.out::println);
+            Stream<String> sm= Stream.of("Syam","Syamala","Revanth");
+
+//            The third method signature is used when we are dealing with different types
+            String s=sm.parallel().reduce("Bayyana ",(ss,p)->ss+" ".concat(p+", "),(s1,s2)->s1.concat(s2));
+            logger.info("reduced string:- "+s);
+//            int stringLength=sm.parallel().reduce(0,(ss,p)->ss+p.length(),(s1,s2)->s1+s2);
+//            logger.info("reduced string:- "+stringLength);
+
         }catch (Exception e){
             logger.log(Level.WARNING,"File not found in specefied path"+e);
         }finally {
