bd9f4224b2ffa04346a88c7ced37abd5154b6c05
syamd <syamdinesh@gmail.com>
2021-06-29 13:40:15 +0530

    Java Stream API practice class- collect method usage.
    Signed-off-by: syamd <syamdinesh@gmail.com>

--- SpringbootJunitMokito/src/main/java/com/example/demo/corejava/practice/StreamApi.java
+++ SpringbootJunitMokito/src/main/java/com/example/demo/corejava/practice/StreamApi.java
@@ -3,9 +3,11 @@ package com.example.demo.corejava.practice;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -15,22 +17,30 @@ import java.util.stream.Stream;
 public class StreamApi {
     public static void main(String[] args) {
         Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
-        try{
-//            Files.lines(Paths.get("C:\\Users\\syamd\\OneDrive\\Desktop\\edo.txt")).map(s->s.subSequence(4,s.length())).forEach(System.out::println);
-            Stream<String> sm= Stream.of("Syam","Syamala","Revanth");
-
+        try {
+            String filePath="C:\\Users\\syamd\\OneDrive\\Desktop\\edo.txt";
+            String string = Files.lines(Paths.get(filePath)).parallel().reduce("Bayyana ", (s, y) -> s.concat(y.substring(2, y.length())) + ", ", String::concat);
+            List<String> listOfStrings = Files.lines(Paths.get(filePath)).collect(Collectors.toList());
+            listOfStrings.forEach(System.out::println);
+            logger.info(string);
+            IntSummaryStatistics collect = Files.lines(Paths.get(filePath)).collect(Collectors.summarizingInt(s -> s.length()));
+            logger.info("this is intSummary:- "+collect);
+            
+//            Stream<String> sm= Stream.of("Syam","Syamala","Revanth","Narayana","Ashwath");
 //            The third method signature is used when we are dealing with different types
-            String s=sm.parallel().reduce("Bayyana ",(ss,p)->ss+" ".concat(p+", "), String::concat);
-            logger.info("reduced string:- "+s);
-//            int stringLength=sm.parallel().reduce(0,(ss,p)->ss+p.length(),(s1,s2)->s1+s2);
+//            String s=sm.parallel().reduce("Bayyana ",(ss,p)->ss+" ".concat(p+", "), String::concat);
+
+//            Files.write(Paths.get("C:\\Users\\syamd\\OneDrive\\Desktop\\edo1.txt"), string.getBytes());
+//            logger.info("reduced string:- "+s);
+//            int stringLength=sm.parallel().reduce(0,(ss,p)->p.indexOf(p),(s1,s2)->s1+s2);
 //            logger.info("reduced string:- "+stringLength);
 
-        }catch (Exception e){
-            logger.log(Level.WARNING,"File not found in specefied path"+e);
-        }finally {
-            List<String> list= Arrays.asList("Syam", "Dinesh","Bayyana");
-            int count= (int) list.stream().filter(s->s.startsWith("B")).peek(System.out::print).count();
-            logger.log(Level.INFO,"Matched string count:- "+count);
+        } catch (Exception e) {
+            logger.log(Level.WARNING, "File not found in specefied path" + e);
+        } finally {
+            List<String> list = Arrays.asList("Syam", "Dinesh", "Bayyana");
+            int count = (int) list.stream().filter(s -> s.startsWith("B")).peek(System.out::print).count();
+            logger.log(Level.INFO, "Matched string count:- " + count);
         }
     }
 }
