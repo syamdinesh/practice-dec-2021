d85553b3fc855d0adf73155216fe07f8ad5f7bb5
syamd <syamdinesh@gmail.com>
2021-07-02 23:14:18 +0530

    exception practice

---
+++ SpringbootJunitMokito/src/main/java/com/example/demo/corejava/practice/excpetions/ExceptionPractice.java
@@ -0,0 +1,29 @@
+package com.example.demo.corejava.practice.excpetions;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class ExceptionPractice {
+    public static void main(String[] args) {
+        Base b=new Derived();
+        try {
+            b.print();
+        }catch (FileNotFoundException e){
+            System.out.print("AWE");
+        }finally {
+            System.out.print("SOME");
+        }
+    }
+}
+
+abstract class Base{
+    public abstract void print() throws IOException;
+}
+
+class Derived extends Base {
+
+    @Override
+    public void print() throws IOException {
+        throw new FileNotFoundException();
+    }
+}
