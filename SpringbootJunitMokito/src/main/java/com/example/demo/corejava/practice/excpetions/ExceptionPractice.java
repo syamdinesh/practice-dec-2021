6012316c8a3ac8608e4b3183398f5c7d84a71a79
syamd <syamdinesh@gmail.com>
2021-07-03 21:51:50 +0530

    Exception Practice example-2 Narasimha Karumanchi - Data structures and algorithms made easy-CareerMonk
    Signed-off-by: syamd <syamdinesh@gmail.com>

--- SpringbootJunitMokito/src/main/java/com/example/demo/corejava/practice/excpetions/ExceptionPractice.java
+++ SpringbootJunitMokito/src/main/java/com/example/demo/corejava/practice/excpetions/ExceptionPractice.java
@@ -2,17 +2,22 @@ package com.example.demo.corejava.practice.excpetions;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class ExceptionPractice {
     public static void main(String[] args) {
         Base b=new Derived();
-        try {
-            b.print();
-        }catch (IOException e){
-            System.out.print("AWE");
-        }finally {
-            System.out.print("SOME");
-        }
+//        try {
+//            b.print();
+            ArrayList<String> listOfStrings = new ArrayList<>();
+            listOfStrings.add("A");listOfStrings.add("E");listOfStrings.add("I");listOfStrings.add("O");listOfStrings.add("U");
+            listOfStrings.addAll(listOfStrings.subList(0,4));
+            listOfStrings.forEach(System.out::print);
+//        }catch (IOException e){
+//            System.out.print("AWE");
+//        }finally {
+//            System.out.print("SOME");
+//        }
     }
 }
 
