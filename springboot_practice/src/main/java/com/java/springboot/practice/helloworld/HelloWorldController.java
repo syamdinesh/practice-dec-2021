0c13f01f184f379bd2bccb5c431f70a88b8dd14f
syamd <syamdinesh@gmail.com>
2021-07-28 19:40:13 +0530

    internationalization updated code

--- springboot_practice/src/main/java/com/java/springboot/practice/helloworld/HelloWorldController.java
+++ springboot_practice/src/main/java/com/java/springboot/practice/helloworld/HelloWorldController.java
@@ -4,13 +4,12 @@
 package com.java.springboot.practice.helloworld;
 
 import java.util.List;
-import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.java.springboot.practice.dao.UserDaoService;
@@ -61,9 +60,8 @@ public class HelloWorldController {
 	}
 
 	@GetMapping(path = "/internationalized")
-	public String helloWorldInternationalized(
-			@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
-		return messageSource.getMessage("good.morning.message", null, locale);
+	public String helloWorldInternationalized() {
+		return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
 	}
 
 }
